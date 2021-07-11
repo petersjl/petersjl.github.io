f99a143e16afd780f18b3621bc8d24c9d889af9d
Peters-Joseph <petersjl@rose-hulman.edu>
2021-07-11 00:39:24 -0400

    Main setup

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * pjl
+ */
+
+/** namespace. */
+var pjl = pjl || {};
+
+/* Main */
+pjl.main = function () {
+	console.log("Ready");
+};
+
+pjl.main();
